d6b3bb82e197d2360022729086a804049cadceed
parhamsaremi <pa.sa.13792000@outlook.com>
2021-01-07 21:07:47 +0330

    finished control panel

--- assignments/hw3/front/panel.js
+++ assignments/hw3/front/panel.js
@@ -28,30 +28,112 @@ function loadPostPage(){
   document.getElementById("postPage").style.borderBottom = "thick solid";
   document.getElementById("dataTable").innerHTML = "";
   document.getElementById("smallDataTable").innerHTML = "";
-  document.getElementById("dataGrid").innerHTML = `
-  <span class="card addcard text-white bg-success mb-3" style="width:auto;min-width: 150px;min-height=400px;">
+  addCard = `
+  <span class="card addcard text-white bg-success mb-3" style="width:auto;min-width: 150px;min-height=400px;" onclick="addPost()">
   <div class="card-body">
 
   <?xml version="1.0"?><svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 128 128" width="175px" height="175px" style="margin-top:40%;margin-left:10%">    <path d="M 64 6.0507812 C 49.15 6.0507812 34.3 11.7 23 23 C 0.4 45.6 0.4 82.4 23 105 C 34.3 116.3 49.2 122 64 122 C 78.8 122 93.7 116.3 105 105 C 127.6 82.4 127.6 45.6 105 23 C 93.7 11.7 78.85 6.0507812 64 6.0507812 z M 64 12 C 77.3 12 90.600781 17.099219 100.80078 27.199219 C 121.00078 47.499219 121.00078 80.500781 100.80078 100.80078 C 80.500781 121.10078 47.500781 121.10078 27.300781 100.80078 C 7.0007813 80.500781 6.9992188 47.499219 27.199219 27.199219 C 37.399219 17.099219 50.7 12 64 12 z M 64 42 C 62.3 42 61 43.3 61 45 L 61 61 L 45 61 C 43.3 61 42 62.3 42 64 C 42 65.7 43.3 67 45 67 L 61 67 L 61 83 C 61 84.7 62.3 86 64 86 C 65.7 86 67 84.7 67 83 L 67 67 L 83 67 C 84.7 67 86 65.7 86 64 C 86 62.3 84.7 61 83 61 L 67 61 L 67 45 C 67 43.3 65.7 42 64 42 z"/></svg>
-  </div>
+  </div>`
   
-  </span>
+  infoCard = `</span>
   <span class="card border-light mb-3" style="min-width: 150px;min-height:400px;">
   <div class="card-header">تاریخ</div>
   <div class="card-body" style="position: relative;">
     <h5 class="card-title">فرستنده</h5>
     <p class="card-text">متن نمونه</p>
-    <div style="position: absolute; bottom:10px;">
-      <div  style="position: relative; bottom:10px;">
-        <button type="button" class="btn btn-danger">حذف</button>
-        <button type="button" class="btn btn-warning" style="position: absolute;right: 10px;">تغییر</button>
-      </div>
+    <div class="justify-content-center" style="position: absolute; bottom:10px;">
+      <button type="button" class="btn btn-danger" onclick="deletePost()">حذف</button>
+      <button type="button" class="btn btn-warning" onclick="editPost(this)">تغییر</button>
     </div>
   </div>
 </span>`
 
+document.getElementById("dataGrid").innerHTML = addCard + infoCard
+
 
 }
+
+
+function editPost(elem){
+
+  //get initial value
+
+  initialHeader = elem.parentElement.parentElement.getElementsByTagName('h5')[0].innerHTML
+  initialText = elem.parentElement.parentElement.getElementsByTagName('p')[0].innerHTML
+  Swal.mixin({
+    confirmButtonText: 'بعدی &rarr;',
+    showCancelButton: true,
+    progressSteps: ['1', '2']
+  }).queue([
+    {
+      title: 'عنوان را در صورت نیاز تغییر دهید.',
+      input: 'text',
+      inputValue: initialHeader
+    },{
+      title: 'متن را در صورت نیاز تغییر دهید', 
+      input: 'textarea',
+      inputValue: initialText
+    }
+    
+  ]).then((result) => {
+    if (result.value) {
+      const answers = JSON.stringify(result.value)
+      Swal.fire({
+        title: 'با موفقیت انجام شد.',
+        icon:'success',
+        confirmButtonText: 'خروج!'
+      })
+    }
+  })
+
+}
+
+function addPost(){
+  Swal.mixin({
+    confirmButtonText: 'بعدی &rarr;',
+    showCancelButton: true,
+    progressSteps: ['1', '2']
+  }).queue([
+    {
+      title: 'عنوان را وارد کنید.',
+      input: 'text'
+    },{
+      title: 'متن مورد نظر را وارد کنید', 
+      input: 'textarea'
+    }
+    
+  ]).then((result) => {
+    if (result.value) {
+      const answers = JSON.stringify(result.value)
+      Swal.fire({
+        title: 'با موفقیت انجام شد.',
+        icon:'success',
+        confirmButtonText: 'خروج!'
+      })
+    }
+  })
+}
+
+function deletePost(){
+  Swal.fire({
+    title: 'آیا از حذف پست مطمئنید؟',
+    text: "این عمل غیر قابل برگشت است.",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'بله',
+    cancelButtonText: 'خیر'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'فایل شما با موفقیت حذف شد'
+      )
+    }
+  })
+
+}
+
 
 function loadInfoPage(){
   document.getElementById("infoPage").style.borderBottom = "thick solid";
