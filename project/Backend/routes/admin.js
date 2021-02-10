ec9bf8c4074854f17dcf5965fdb0d9246f530431
AlirezaT99 <tajmirriahia@yahoo.com>
2021-02-10 16:16:17 +0330

    admin API is done for now

--- project/Backend/routes/admin.js
+++ project/Backend/routes/admin.js
@@ -30,7 +30,7 @@ router.delete('/feedback/:id', async (req, res) => {
 });
 
 /**
- * if id is given, that commit will be confirmed,
+ * if id is given, that comment will be confirmed,
  * else, all of them will be confirmed.
  */
 router.put('/confirm/:id?', async (req, res) => {
@@ -44,6 +44,30 @@ router.put('/confirm/:id?', async (req, res) => {
         await db.query("UPDATE comments SET confirmed = TRUE WHERE id = $1", [commentId]);
     }
     return res.sendStatus(204);
+});
+
+/**
+ * returns all users
+ */
+router.get('/users', async (req, res) => {
+
+    const { rows: users } = await db.query("SELECT name, email FROM users")
+
+    return res.status(200).send({ users })
+});
+
+/**
+ * deletes the user with the email specified in query params
+ */
+router.delete('/users', async (req, res) => {
+    const { email } = req.query;
+    if (email === undefined) {
+        return res.status(400).send({ message: "email isn't specified" })
+    }
+    
+    await db.query("DELETE FROM users WHERE email = $1", [email])
+
+    return res.sendStatus(204)
 });
 
 module.exports = router
