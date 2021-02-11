4b9458907bba897d3e934403e2bda18effb6d378
AlirezaT99 <tajmirriahia@yahoo.com>
2021-02-11 22:47:26 +0330

    profs api updated

--- project/Backend/routes/profs.js
+++ project/Backend/routes/profs.js
@@ -1,18 +1,22 @@
 const Router = require('express-promise-router')
+const {query, validationResult} = require('express-validator')
+
 const db = require('../db')
 
 const router = new Router()
 
-router.get('/search', async (req, res) => {
-    const { name } = req.query
-    if (name === undefined) {
-        return res.status(400).send({ message: "name isn't specified" })
-    }
+router.get('/search',
+    query('name').exists(),
+    async (req, res) => {
+        const {name} = req.query
 
-    const response = await db.getProfs(name)
-    let profs = response.hits.hits.map((hit) => hit._source)
+        if (!validationResult(req).isEmpty())
+            return res.status(400).send({message: "name isn't specified"})
 
-    return res.status(200).send({ profs })
-});
+        const response = await db.getProfs(name)
+        let profs = response.hits.hits.map((hit) => hit._source)
+
+        return res.status(200).send({profs})
+    });
 
 module.exports = router
