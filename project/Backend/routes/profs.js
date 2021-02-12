ca3ad7793cd0a6ea4a31fb55290a1e08835883c5
AlirezaT99 <tajmirriahia@yahoo.com>
2021-02-12 11:10:23 +0330

    profs API updated

--- project/Backend/routes/profs.js
+++ project/Backend/routes/profs.js
@@ -1,22 +1,60 @@
 const Router = require('express-promise-router')
-const {query, validationResult} = require('express-validator')
+const { query, param, validationResult } = require('express-validator')
 
 const db = require('../db')
 
 const router = new Router()
 
+/**
+ * returns the results of fuzzy search on profName
+ */
 router.get('/search',
     query('name').exists(),
     async (req, res) => {
-        const {name} = req.query
+        const { name } = req.query
 
         if (!validationResult(req).isEmpty())
-            return res.status(400).send({message: "name isn't specified"})
+            return res.status(400).send({ message: "name isn't specified" })
 
         const response = await db.getProfs(name)
         let profs = response.hits.hits.map((hit) => hit._source)
 
-        return res.status(200).send({profs})
+        return res.status(200).send({ profs })
+    });
+
+/**
+ * returns prof info
+ */
+router.get('/:id',
+    param('id').isInt(),
+    async (req, res) => {
+        const { id: profId } = req.params
+
+        if (!validationResult(req).isEmpty())
+            return res.status(400).send({ message: "profId isn't correct" })
+
+        const { rows: info } = await db.query("SELECT fa_name, uni, image_path "
+            + "FROM profs WHERE ID = $1", [profId])
+
+        return res.status(200).send({ info })
+    });
+
+/**
+ * returns a prof's average ratings
+ */
+router.get('/:id',
+    param('id').isInt(),
+    async (req, res) => {
+        const { id: profId } = req.params
+
+        if (!validationResult(req).isEmpty())
+            return res.status(400).send({ message: "profId isn't correct" })
+
+        // TODO add columns to replace * with their avg and count in the following query
+        const { rows: info } = await db.query("SELECT * FROM comments "
+            + "WHERE PROF_ID = $1 AND CONFIRMED = TRUE", [profId])
+
+        return res.status(200).send({ info })
     });
 
 module.exports = router
