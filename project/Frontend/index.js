7dde6c7b8b775347f9b14ea967a60f57febc56b8
parhamsaremi <pa.sa.13792000@outlook.com>
2021-02-12 16:19:44 +0330

    minor bug in login page

--- project/Frontend/index.js
+++ project/Frontend/index.js
@@ -6,7 +6,7 @@ showHome = function(){
     let homeButton = document.getElementById("homeBtn")
     let loginButton = document.getElementById("loginBtn")
     homeButton.classList.add("selected")
-    loginButton.classList.remove("selcted")
+    loginButton.classList.remove("selected")
     document.getElementById("welcomPage").style.display = "flex"
     document.getElementById("loginPage").style.display = "none"
 }
