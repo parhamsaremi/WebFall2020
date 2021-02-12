cc93eb7394a70259cca8b6c0d050cd3c28b66dbb
parhamsaremi <pa.sa.13792000@outlook.com>
2021-02-12 15:32:15 +0330

    finished login

---
+++ project/Frontend/index.js
@@ -0,0 +1,23 @@
+load = function(){
+    showHome()
+}
+
+showHome = function(){
+    let homeButton = document.getElementById("homeBtn")
+    let loginButton = document.getElementById("loginBtn")
+    homeButton.classList.add("selected")
+    loginButton.classList.remove("selcted")
+    document.getElementById("welcomPage").style.display = "flex"
+    document.getElementById("loginPage").style.display = "none"
+}
+
+
+showLogin = function(){
+    let homeButton = document.getElementById("homeBtn")
+    let loginButton = document.getElementById("loginBtn")
+    homeButton.classList.remove("selected")
+    loginButton.classList.add("selected")
+    document.getElementById("welcomPage").style.display = "none"
+    document.getElementById("loginPage").style.display = "block"
+
+}
