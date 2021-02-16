99d39e08b9102b59bdf6b6111ee777baf0e8aaff
parhamsaremi <pa.sa.13792000@outlook.com>
2021-02-16 13:25:25 +0330

    fixed buttons

--- project/Frontend/index.js
+++ project/Frontend/index.js
@@ -8,8 +8,10 @@ showHome = function(){
     let loginButton = document.getElementById("loginBtn")
     homeButton.classList.add("selected")
     loginButton.classList.remove("selected")
-    document.getElementById("welcomPage").style.display = "flex"
+    document.getElementById("welcomePage").style.display = "flex"
     document.getElementById("loginPage").style.display = "none"
+    document.getElementById("teacher_container").style.display="none"
+
 }
 
 
@@ -18,8 +20,10 @@ showLogin = function(){
     let loginButton = document.getElementById("loginBtn")
     homeButton.classList.remove("selected")
     loginButton.classList.add("selected")
-    document.getElementById("welcomPage").style.display = "none"
+    document.getElementById("welcomePage").style.display = "none"
     document.getElementById("loginPage").style.display = "block"
+    document.getElementById("teacher_container").style.display="none"
+
 
 }
 
@@ -49,4 +53,10 @@ teacherCharts = function(){
     document.getElementById("tCharts").classList.add("active")
     document.getElementById("tComments").classList.remove("active")
     document.getElementById("tHome").classList.remove("active")
+}
+
+
+search = function(){
+    document.getElementById("welcomePage").style.display="none"
+    document.getElementById("teacher_container").style.display="flex"
 }
