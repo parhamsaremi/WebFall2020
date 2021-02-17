cca9c9f3dff610c7139318ef4ae8ad3f7850a380
parhamsaremi <pa.sa.13792000@outlook.com>
2021-02-17 18:11:44 +0330

    finished panel for add prof

--- project/Frontend/admin.js
+++ project/Frontend/admin.js
@@ -9,3 +9,14 @@ login = function(){
     document.getElementById("loginPage").style.display="none"
     document.getElementById("newProfPanel").style.display="none"
 }
+
+cancelProf = function(){
+    document.getElementById("admin_container").style.display= "flex"
+    document.getElementById("newProfPanel").style.display="none"
+}
+
+addProf = function(){
+    // TODO send data to backend
+    document.getElementById("admin_container").style.display= "flex"
+    document.getElementById("newProfPanel").style.display="none"
+}
