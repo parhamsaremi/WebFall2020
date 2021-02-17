8ba9cf91dd2f4a67e1c838b050707d6c35b78c80
AlirezaT99 <tajmirriahia@yahoo.com>
2021-02-17 12:46:38 +0330

    prof search improved

--- project/Frontend/index.js
+++ project/Frontend/index.js
@@ -1,62 +1,82 @@
-load = function(){
+load = function () {
     // showHome()
     fillChart()
 }
 
-showHome = function(){
+showHome = function () {
     let homeButton = document.getElementById("homeBtn")
     let loginButton = document.getElementById("loginBtn")
     homeButton.classList.add("selected")
     loginButton.classList.remove("selected")
     document.getElementById("welcomePage").style.display = "flex"
     document.getElementById("loginPage").style.display = "none"
-    document.getElementById("teacher_container").style.display="none"
+    document.getElementById("teacher_container").style.display = "none"
 
 }
 
 
-showLogin = function(){
+showLogin = function () {
     let homeButton = document.getElementById("homeBtn")
     let loginButton = document.getElementById("loginBtn")
     homeButton.classList.remove("selected")
     loginButton.classList.add("selected")
     document.getElementById("welcomePage").style.display = "none"
     document.getElementById("loginPage").style.display = "block"
-    document.getElementById("teacher_container").style.display="none"
+    document.getElementById("teacher_container").style.display = "none"
 
 
 }
 
-fillChart= function(){
+fillChart = function () {
 
 }
 
-teacherHome = function(){
+teacherHome = function () {
     document.getElementById("tHome").classList.add("active")
     document.getElementById("tComments").classList.remove("active")
     document.getElementById("tCharts").classList.remove("active")
-    document.getElementById("overview").style.display="block"
-    document.getElementById("comments").style.display= "none"
+    document.getElementById("overview").style.display = "block"
+    document.getElementById("comments").style.display = "none"
 
 }
 
-teacherComments = function(){
+teacherComments = function () {
     document.getElementById("tComments").classList.add("active")
     document.getElementById("tHome").classList.remove("active")
     document.getElementById("tCharts").classList.remove("active")
-    document.getElementById("comments").style.display= "block"
-    document.getElementById("overview").style.display="none"
+    document.getElementById("comments").style.display = "block"
+    document.getElementById("overview").style.display = "none"
 
 }
 
-teacherCharts = function(){
+teacherCharts = function () {
     document.getElementById("tCharts").classList.add("active")
     document.getElementById("tComments").classList.remove("active")
     document.getElementById("tHome").classList.remove("active")
 }
 
+function checkForEnter(event) {
+    if (event.key === 'Enter')
+        search()
+}
 
-search = function(){
-    document.getElementById("welcomePage").style.display="none"
-    document.getElementById("teacher_container").style.display="flex"
+search = function () {
+    let queryName = document.getElementById('form1').value;
+    
+    if (queryName.length < 3)
+        return;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status === 200) {
+            const { profs } = xhttp.response; // response: {"profs":[{"name":"Kharazi","id":2},{"name":"Kharrazi","id":1}]}
+            console.log(profs)
+            // front
+            document.getElementById("welcomePage").style.display = "none"
+            document.getElementById("teacher_container").style.display = "flex"
+        }
+    };
+    xhttp.open("GET", `http://localhost:3000/api/profs/search?name=${queryName}`);
+    xhttp.responseType = 'json';
+    xhttp.send();
+
 }
