36e609068d745d889bb9601c9ae12a4b498d9d5e
AlirezaT99 <tajmirriahia@yahoo.com>
2021-02-17 19:03:34 +0330

    home deleted

--- project/Frontend/index.js
+++ project/Frontend/index.js
@@ -23,19 +23,10 @@ showLogin = function () {
     document.getElementById("teacher_container").style.display = "none"
 }
 
-teacherHome = function () {
-    document.getElementById("tHome").classList.add("active")
-    document.getElementById("tComments").classList.remove("active")
-    document.getElementById("tCharts").classList.remove("active")
-    document.getElementById("overview").style.display = "block"
-    document.getElementById("comments").style.display = "none"
-}
-
 teacherComments = function () {
     fetchComments(1); // TODO replace with prof id
     
     document.getElementById("tComments").classList.add("active")
-    document.getElementById("tHome").classList.remove("active")
     document.getElementById("tCharts").classList.remove("active")
     document.getElementById("comments").style.display = "block"
     document.getElementById("overview").style.display = "none"
@@ -44,7 +35,6 @@ teacherComments = function () {
 teacherCharts = function () {
     document.getElementById("tCharts").classList.add("active")
     document.getElementById("tComments").classList.remove("active")
-    document.getElementById("tHome").classList.remove("active")
 
     fetchRatings(1); // TODO replace with prof id
 }
