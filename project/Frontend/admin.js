d241e98cb55d51b8a65fc3cbd3d8237bc7a11550
parhamsaremi <pa.sa.13792000@outlook.com>
2021-02-17 17:39:12 +0330

    function for login

--- project/Frontend/admin.js
+++ project/Frontend/admin.js
@@ -1,3 +1,10 @@
 newProf = function(){
-    
+    document.getElementById("admin_container").style.display="none"
+    document.getElementById("newProfPanel").style.display="block"
+}
+
+
+login = function(){
+    document.getElementById("admin_container").style.display="flex"
+    document.getElementById("loginPage").style.display="none"
 }
