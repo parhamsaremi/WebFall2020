50876384dede895e45d55cea2ef37d04481c22f3
Alireza Tajmir <45307044+AlirezaT99@users.noreply.github.com>
2021-02-18 01:28:38 +0330

    Update admin.js

--- project/Backend/routes/admin.js
+++ project/Backend/routes/admin.js
@@ -10,11 +10,11 @@ const isNumeric = (value) => /^-?\d+$/.test(value);
  */
 router.get('/unconfirmed', async (req, res) => {
 
-    const { rows: comments } = await db.query("SELECT id, name, comment FROM "
+    const {rows: comments} = await db.query("SELECT id, name, comment FROM "
         + "comments INNER JOIN users ON comments.user_email = users.email "
         + "WHERE confirmed = FALSE")
 
-    return res.status(200).send({ comments })
+    return res.status(200).send({comments})
 });
 
 /**
@@ -22,13 +22,13 @@ router.get('/unconfirmed', async (req, res) => {
  * else, all of them will be confirmed.
  */
 router.put('/confirm/:id?', async (req, res) => {
-    const { id: commentId } = req.params
+    const {id: commentId} = req.params
 
     if (commentId === undefined) {
         await db.query("UPDATE comments SET confirmed = TRUE");
     } else {
         if (!isNumeric(commentId))
-            return res.status(400).send({ message: 'comment id is not valid' })
+            return res.status(400).send({message: 'comment id is not valid'})
         await db.query("UPDATE comments SET confirmed = TRUE WHERE id = $1", [commentId]);
     }
     return res.sendStatus(204);
@@ -39,19 +39,19 @@ router.put('/confirm/:id?', async (req, res) => {
  */
 router.get('/users', async (req, res) => {
 
-    const { rows: users } = await db.query("SELECT name, email FROM users")
+    const {rows: users} = await db.query("SELECT name, email FROM users")
 
-    return res.status(200).send({ users })
+    return res.status(200).send({users})
 });
 
 /**
  * deletes the comment with given id
  */
 router.delete('/feedback/:id', async (req, res) => {
-    const { id: commentId } = req.params
+    const {id: commentId} = req.params
 
     if (!isNumeric(commentId))
-        return res.status(400).send({ message: 'comment id is not valid' })
+        return res.status(400).send({message: 'comment id is not valid'})
 
     await db.query("DELETE FROM comments WHERE id = $1", [commentId]);
 
@@ -62,9 +62,9 @@ router.delete('/feedback/:id', async (req, res) => {
  * deletes the user with the email specified in query params
  */
 router.delete('/users', async (req, res) => {
-    const { email } = req.query;
+    const {email} = req.query;
     if (email === undefined) {
-        return res.status(400).send({ message: "email isn't specified" })
+        return res.status(400).send({message: "email isn't specified"})
     }
 
     await db.query("DELETE FROM users WHERE email = $1", [email])
@@ -77,8 +77,14 @@ router.delete('/users', async (req, res) => {
  */
 router.post('/profs', async (req, res) => {
     const { faName, enName, imagePath, uni } = req.body;
-    await db.query("INSERT INTO profs (fa_name, en_name, image_path, uni) "
-        + "VALUES ($1, $2, $3, $4)", [faName, enName, imagePath, uni])
+
+    const {rows: idArr} = await db.query("SELECT MAX(id) FROM profs");
+    let id = 1 + idArr[0].max;
+
+    await db.query("INSERT INTO profs (id, fa_name, en_name, image_path, uni) "
+        + "VALUES ($1, $2, $3, $4, $5)", [id, faName, enName, imagePath, uni])
+
+    await db.addProf(faName, imagePath, id)
 
     return res.sendStatus(204)
 });
@@ -88,9 +94,9 @@ router.post('/profs', async (req, res) => {
  */
 router.get('/requests', async (req, res) => {
 
-    const { rows: requests } = await db.query("SELECT * FROM requests")
+    const {rows: requests} = await db.query("SELECT * FROM requests")
 
-    return res.status(200).send({ requests })
+    return res.status(200).send({requests})
 });
 
 
